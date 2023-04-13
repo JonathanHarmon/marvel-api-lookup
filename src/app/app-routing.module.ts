2b42287f5b48358c2c52a09867892fc71ad1ec3b
Jonathan Harmon <jonathan@jonathanharmon.com>
2023-04-13 06:30:30 -0400

    updated routing & remvd bootstrap link from index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterDetailComponent } from './components/character-detail/character-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'character-list', component: CharacterListComponent},
+  { path: 'character-detail/:id', component: CharacterDetailComponent},
+  { path: '**', redirectTo: 'character-list'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
