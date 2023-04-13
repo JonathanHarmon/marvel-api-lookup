65204837f52289dc28f68f8829eac2a639d4ffb6
Jonathan Harmon <jonathan@jonathanharmon.com>
2023-04-13 06:06:16 -0400

    created api-connector service

--- src/app/services/api-connector.service.ts
+++ src/app/services/api-connector.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiConnectorService {
 
-  constructor() { }
+  apiUrl = environment.apiUrl;
+  public getJsonValue: any;
+  public postJsonValue: any;
+  constructor(private http: HttpClient){
+
+  }
+
+
+
+  public getMethod(){
+    this.http.get(this.apiUrl).subscribe((data) => {
+      console.log(data);
+      this.getJsonValue = data;
+    });
+  }
 }
+
