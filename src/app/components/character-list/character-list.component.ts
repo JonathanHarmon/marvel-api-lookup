04c8f1c510773469a14189e99a494fa9cddf1316
Jonathan Harmon <jonathan@jonathanharmon.com>
2023-04-14 06:40:04 -0400

    update api service & injected into character-list

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiConnectorService } from 'src/app/services/api-connector.service';
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
   styleUrls: ['./character-list.component.css']
 })
-export class CharacterListComponent {
+export class CharacterListComponent implements OnInit{
+
+  constructor(private apiConnectorService: ApiConnectorService){}
+
+  ngOnInit(): void {
+      this.apiConnectorService.getCharacters('thor').subscribe(
+        (results:any) => {
+          console.log(results);
+        }
+      )
+  }
 
 }
--- src/app/services/api-connector.service.ts
+++ src/app/services/api-connector.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiConnectorService {
 
-  apiUrl = 'http://gateway.marvel.com/v1/public/characters?';
-  apiKey = environment.apiKey;
+  apiUrl: string = 'http://gateway.marvel.com/v1/public/characters?';
+  apiKey: string = environment.apiKey;
   public getJsonValue: any;
   public postJsonValue: any;
   constructor(private http: HttpClient){}
 
-  public getCharacters(){
-    this.http.get(`${this.apiUrl}${this.apiKey}`).subscribe((data) => {
-      console.log(data);
-      this.getJsonValue = data;
-    });
+  public getCharacters(keyword:string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}nameStartsWith=${keyword}&${this.apiKey}`);
   }
 
 
